16a07e0cfc08de37655d6d6c41a0e7cc080d8a5b
mkanczuzewski <mkanczuzewski@gmail.com>
2021-10-23 09:15:44 -0400

    flow from start to first question

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,20 @@
-// Push in start text and start button; hide other three buttons
-// var body = document.body;
-// var startArea = document.createElement("div")
-// var startTitle = document.createElement("h1");
-// var startDesc = document.createElement("div");
-// var startButton = document.createElement("button");
+// on click hide start
+const startJS = document.getElementById("startId");
+const startBtnJS = document.getElementById("startBtn");
+const questionJS = document.getElementById("questionId")
 
-// startTitle.textContent = "Coding Quiz Challenge";
-// startDesc.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!";
-// startButton.textContent = "Start Quiz";
-
-// body.appendChild(startArea)
-// startArea.appendChild(startTitle);
-// startArea.appendChild(startDesc);
-// startArea.appendChild(startButton);
-
-// startArea.setAttribute("style", "display:flex; flex-direction:column; justify-content:center; align-items:center;");
-// startTitle.setAttribute("style", "text-align:center; font-size:50px");
-// startDesc.setAttribute("style", "text-align:center; font-size:25px");
-// startButton.setAttribute("style", "color:white; background-color:blue; border-radius: 12px; margin:10px; padding: 15px 32px; font-size: 16px;");
-// startButton:hover.setAttribute("style", "color:white; background-color:teal; border-radius: 12px; margin:10px; padding: 15px 32px; font-size: 16px;");
-// hover button state
-
+startBtnJS.onclick = function () {
+   if (startJS.style.display !== "none") {
+     startJS.style.display = "none";
+     questionJS.style.display = "flex"
+   } else {
+     startJS.style.display = "flex";
+   }
+};
 // on click start timer
 
 // on click change text to first question in array
-    // var a1E1 = document.createElement("button")
+
 // on click of correct answer, state its correct on that screen, then change to next question
 
 // else on click of wrong answer, state it is wrong, deduct 10seconds, continue timer, and go to next question
